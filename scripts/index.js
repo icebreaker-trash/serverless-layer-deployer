6b706e75512870921723cad1497727499a3e3b92
icebreaker99 <qq1324318532@gmail.com>
2021-01-06 14:54:20 +0800

    feat: refactor util

--- scripts/index.js
+++ scripts/index.js
@@ -11,54 +11,59 @@ const {
   TENCENT_COS_LAYER_BUCKET,
   TENCENT_COS_LAYER_REGION
 } = process.env
-const layerPath = '.serverless\\node_modules.zip'
-const Bucket = 'sls-layer-ap-shanghai-code-1257725330'
+const layerPath = '.serverless/layer/node_modules.zip'
+const Bucket = 'sls-layer-ap-shanghai-code'
 const Key = 'node_modules.zip'
+const execa = require('execa')
 async function main () {
   // const dirpath = path.resolve(__dirname, '../node_modules')
+  // const destpath = path.resolve(__dirname, '../.serverless/layer/node_modules.zip')
   // const {
   //   path: layerPath
-  // } = await compress(dirpath)
-
+  // } = await compress(dirpath, destpath)
+  // console.log(layerPath)
   // const fileName = path.basename(layerPath)
   // console.log(fileName)
-  // const {
-  //   Bucket,
-  //   Key
-  // } = await tencent.upload({
-  //   Bucket: TENCENT_COS_LAYER_BUCKET,
-  //   FilePath: layerPath,
-  //   Key: fileName,
-  //   Region: TENCENT_COS_LAYER_REGION,
-  //   SecretId: TENCENT_SECRET_ID,
-  //   SecretKey: TENCENT_SECRET_KEY
-  // })
-
-  // tencent.generateYml({
-  //   name: 'layer-test-202101',
-  //   inputs: {
-  //     name: 'sls-layer-deploy-test',
-  //     region: 'ap-shanghai',
-  //     src: {
-  //       bucket: Bucket,
-  //       object: '/' + Key
-  //       // exclude:[
-  //       //   'sss'
-  //       // ]
-  //     },
-  //     description: 'auto deploy by serverless-layer-deployer'
-  //   }
-  // })
+  const fileName = 'node_modules.zip'
+  const {
+    Bucket,
+    Key
+  } = await tencent.upload({
+    Bucket: TENCENT_COS_LAYER_BUCKET,
+    FilePath: layerPath,
+    Key: fileName,
+    Region: TENCENT_COS_LAYER_REGION,
+    SecretId: TENCENT_SECRET_ID,
+    SecretKey: TENCENT_SECRET_KEY
+  })
+  console.log(Bucket, Key)
+  tencent.generateYml({
+    name: 'layer-test-202101',
+    inputs: {
+      name: 'sls-layer-deploy-test',
+      region: 'ap-shanghai',
+      src: {
+        bucket: Bucket,
+        object: Key
+        // exclude:[
+        //   'sss'
+        // ]
+      },
+      description: 'auto deploy by serverless-layer-deployer'
+    }
+  })
 
   // console.log("i'm the last")
 
-  process.chdir('./.serverless/layer')
-  try {
-    const res = await tencent.serverlessDeploy()
-    console.log(res)
-  } catch (err) {
-    console.log(err)
-  }
+  // process.chdir('./.serverless/layer')
+  // try {
+  //   const res = await tencent.serverlessDeploy()
+  //   console.log(res)
+  // } catch (err) {
+  //   console.log(err)
+  // }
+  // const res = await execa('serverless', ['-h'])
+  // console.log(res.stdout)
 }
 
 main()
--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ const zip = require('..')
 const fs = require('fs')
 describe('lib/index.js', () => {
   test('default', async () => {
-    await zip()
+    await zip('node_modules', '.serverless/layer/node_modules.zip')
     expect(fs.existsSync('node_modules.zip')).toBe(true)
   })
 })
